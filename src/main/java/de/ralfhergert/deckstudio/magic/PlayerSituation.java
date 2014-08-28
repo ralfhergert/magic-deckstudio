82e164253847cf931a977cac04b8fda2ef764bec
Ralf Hergert <ralf.hergert@haufe-lexware.net>
2014-08-28 09:03:17 +0200

    added hasPlayedLand flag

--- src/main/java/de/ralfhergert/deckstudio/magic/PlayerSituation.java
+++ src/main/java/de/ralfhergert/deckstudio/magic/PlayerSituation.java
@@ -10,6 +10,7 @@ public class PlayerSituation {
 
     private int lifePoints = 20;
     private List<Mana> manaPool = new ArrayList<Mana>();
+    private boolean hasPlayedLand = false;
     private List<Card> hand = new ArrayList<Card>();
     private List<Card> library = new ArrayList<Card>();
     private List<Card> graveyard = new ArrayList<Card>();
